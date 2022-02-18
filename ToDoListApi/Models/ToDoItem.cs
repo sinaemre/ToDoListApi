68e1dc768221302e6e4a593953d3261906ce52e0
Sina Emre <snabkr7010@gmail.com>
2022-02-18 12:51:55 +0300

    Add project files.

---
+++ ToDoListApi/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApi.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
